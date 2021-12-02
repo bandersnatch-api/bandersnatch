c6f777d25db440fc1571b2d2bcabaf8baeef1771
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-02 13:04:25 +0000

    Changing drivers

--- bandersnatch-react/src/cards/cards.js
+++ bandersnatch-react/src/cards/cards.js
@@ -1,21 +1,40 @@
 import './Cards.css';
 
-var object = 
-    {
-    "id": 1,
-    "question": "What colour is the sky?",
-    "answer1": "Blue",
-    "answer2": "Red",
-    "answer3": "Green",
-    "answer4": "Yellow",
-    "correct_answer": "Blue",
-    "created_at": "2021-12-01T17:19:15.391Z",
-    "updated_at": "2021-12-01T17:19:15.391Z"
-    }
+// const fetch = require("node-fetch");
+// const url = "https://bandersnatch-api.herokuapp.com/quizzes";
 
-const json = JSON.stringify(object)
+// const getData = async url => {
+//   try {
+//     const response = await fetch(url);
+//     const json = await response.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const parsed = JSON.parse(json)
+fetch('https://bandersnatch-api.herokuapp.com/quizzes')
+.then(response => response.json)
+.then(data => console.log(data))
+
+// json = getData(url);
+
+// var object = 
+//     {
+//     "id": 1,
+//     "question": "What colour is the sky?",
+//     "answer1": "Blue",
+//     "answer2": "Red",
+//     "answer3": "Green",
+//     "answer4": "Yellow",
+//     "correct_answer": "Blue",
+//     "created_at": "2021-12-01T17:19:15.391Z",
+//     "updated_at": "2021-12-01T17:19:15.391Z"
+//     }
+
+// const json = JSON.stringify(object)
+
+// const parsed = JSON.parse(json)
 
 function Cards() {
   return (
@@ -23,8 +42,8 @@ function Cards() {
       <div className="questionDisplay">
         Question 1 : What should you use to store 1, 2 and 4 ? 
         <br/>
-        <button className="anwser1">{ parsed.answer1 }</button>
-        <button className="anwser2">{ parsed.answer2 }</button>
+        <button className="anwser1"></button>
+        <button className="anwser2"></button>
 
       </div>
     </div>
