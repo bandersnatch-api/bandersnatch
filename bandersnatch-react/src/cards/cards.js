f86130fcd1369877ce7824b48a20d5d2774a6f13
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-12-08 11:15:18 +0000

    resolved conflicts

--- bandersnatch-react/src/cards/cards.js
+++ bandersnatch-react/src/cards/cards.js
@@ -1,41 +1,111 @@
-
 import './Cards.css';
 import React, { useState, useEffect } from 'react';
 
-function Cards() {
+
+var userID = 1
+var current_score = 0
+var current_questions_answered = 0
+
+
+function Card() {
   const [questions, setQuestions] = useState(0);
 
   useEffect(() => {
     const fetchQuestions = async () => {const res = await fetch("http://bandersnatch-api.herokuapp.com/quizzes")
     const data = await res.json()
     setQuestions(data)
+    console.log(data)
    }
-
   fetchQuestions()
+  fetchScore()
 
   },[])
 
-  return (
-    <div className="cardContainer">
-      <div className="questionDisplay">
-      {questions && questions[0].question}
+  function updateQuestions() {
+    let newArray = questions.slice(1)
 
-        <button className="anwser1">
-        {questions && questions[0].answer1}
-        </button>
-        <button className="anwser2">
-        {questions && questions[0].answer2}
-        </button>
-        <button className="anwser3">
-        {questions && questions[0].answer3}
-        </button>
-        <button className="anwser4">
-        {questions && questions[0].answer4}
-        </button>
+    setQuestions(newArray)
+  }
 
-      </div>
-    </div>
+  function updateScore(answer) {
+
+    if (answer === questions[0].correct_answer) {
+
+      current_score += 1
+      current_questions_answered +=1 
+      
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ score: `${current_score}`, questions_answered: `${current_questions_answered}` })
+
+
+    };
+    fetch(`https://safe-sea-12739.herokuapp.com/https://bandersnatch-api.herokuapp.com/users/${userID}`, requestOptions)
+    } else if (answer !== questions[0].correct_answer) {
+
+      current_questions_answered +=1 
+      
+      const requestOptions  = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ score: `${current_score}`, questions_answered: `${current_questions_answered}` })
+
+    }
+    fetch(`https://safe-sea-12739.herokuapp.com/https://bandersnatch-api.herokuapp.com/users/${userID}`, requestOptions)
+    }
+  }
+
+  const fetchScore = async () => {const res = await fetch("http://bandersnatch-api.herokuapp.com/users/1")
+  const data = await res.json()
+  current_score = data.score
+  current_questions_answered = data.questions_answered
+
+  }
+
+  function pressButton(answer) {
+    updateQuestions()
+    updateScore(answer)
+    // fetchScore()
+  }
+
+
+  return questions.length > 0 ? (
+    <div className="block mx-auto px-6 	max-width: 768px bg-purple-300">
+
+<div className="cardContainer p-6 mx-auto">
+
+  <div className="bg-white text-pink-300 p-10 rounded-lg shadow-md">
+    <h2 className="text-2-xl font-bold ">
+      {questions[0].question}
+    </h2>
+  </div>
+
+  <div className="grid grid-cols-2 gap-6 mt-6">
+
+    <button onClick={() => pressButton(questions[0].answer1)} className="bg-white p-4
+    text-pink-300 font-semibold rounded shadow">
+    {questions[0].answer1}
+    </button>
+    <button onClick={() => pressButton(questions[0].answer2)} className="bg-white p-4
+    text-pink-300 font-semibold rounded shadow">
+    {questions[0].answer2}
+    </button>
+    <button onClick={() => pressButton(questions[0].answer3)} className="bg-white p-4
+    text-pink-300 font-semibold rounded shadow">
+    {questions[0].answer3}
+    </button>
+    <button onClick={() => pressButton(questions[0].answer4)} className="bg-white p-4
+    text-pink-300 font-semibold rounded shadow">
+    {questions[0].answer4}
+    </button>
+  </div>
+</div>
+</div>
+  ) : (
+    <h2 className="text-2xl text-white -font-bold">Loading... Hold tight!</h2>
   )
-}
+  }
 
-export default Cards
+
+export default Card
