f464d20b100af5a17da1a08c86085605dba0a866
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-09 16:44:00 +0000

    Conditional routing not working as current score set as 0

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import './App.css';
 import Card from './card/Card';
 import StartPage from './StartPage/StartPage';
 import Finalcard from './finalcard/Finalcard'
+import Losecard from './finalcard/Losecard'
 import React from 'react';
 import { BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+import current_score from './card/Card'
 
 function App() {
+
+  function chooseEnding() {
+    if (current_score >3) {
+      console.log(current_score)
+      return <Route path="/finalcard" element={<Finalcard/>} />
+    }
+    else {
+      console.log(current_score)
+      return <Route path="/losecard" element={<Losecard/>} />
+    }
+  }
+
   return (
     <Router>
     <div className="App">
       <Routes>
         <Route path="/" element={<StartPage/>} />
         <Route path="/play" element={<Card/>} />
-        <Route path="/finalcard" element={<Finalcard/>} />
+        {/* <Route path="/finalcard" element={<Finalcard/>} /> */}
+        {chooseEnding()}
       </Routes>
     </div>
     </Router>
---
+++ src/finalcard/Losecard.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Finalcard() {
+  const [questions, setQuestions] = useState(1);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {const res = await fetch("https://safe-sea-12739.herokuapp.com/https://bandersnatch-api.herokuapp.com/quizzes")
+    const data = await res.json()
+    console.log("hello")
+    setQuestions(data)
+    console.log(data)
+   }
+  fetchQuestions()
+
+  },[])
+
+  return questions.length > 0 ? (
+
+<div className="block mx-auto px-6 	max-width: 768px bg-red-800">
+      <div className="cardContainer p-6 mx-auto">
+
+          <div className="storyContainer  p-10 mx-auto" > 
+
+          </div>
+
+      <div className="bg-white text-red-800 p-10
+    rounded-lg shadow-md">
+        <h1 className="text-2xl text-center font-bold">You cannot leave the Labyrinth!</h1> <br></br>
+        <h2>
+          <div className="row">
+            <div className="col-md-4 col-xs-6">
+              <img src="minotaur_image.jpeg" className="mx-auto d-blockmx-auto d-block img-responsive" />
+              <br></br>
+            </div>
+            </div>
+          {questions[5].story}
+        </h2>
+      </div>
+
+      </div>
+    </div>
+  
+) : (
+  <h2 className="text-2xl text-white -font-bold">Loading... Hold tight!</h2>
+)
+;
+}
+export default Finalcard
